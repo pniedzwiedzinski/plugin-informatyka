2b0001697cac6c9d4c49d775458d037b33bc1ee9
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-05-17 16:35:40 +0200

    Add .xlsx

--- functions/informatyka.fish
+++ functions/informatyka.fish
@@ -1,10 +1,12 @@
 function informatyka -d "My package"
   cd /Users/pniedzwiedzinski/Documents/Projekty/informatyka
-  echo "git --git-dir=/Users/pniedzwiedzinski/.informatyka/git status -u | grep .cpp | zip inf \$(cat)" | bash
+  echo "git --git-dir=/Users/pniedzwiedzinski/.informatyka/git status -u | grep '.cpp\|.xlsx' | zip inf \$(cat)" | bash
   mv inf.zip /Users/pniedzwiedzinski/.informatyka
+  
   cd /Users/pniedzwiedzinski/.informatyka
   pipenv run main.py
   cd /Users/pniedzwiedzinski/Documents/Projekty/informatyka
+  
   git --git-dir=/Users/pniedzwiedzinski/.informatyka/git add .
   git --git-dir=/Users/pniedzwiedzinski/.informatyka/git commit -m "Informatyka - dodano automatycznie"
   git --git-dir=/Users/pniedzwiedzinski/.informatyka/git push
